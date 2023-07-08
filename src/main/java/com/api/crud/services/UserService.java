e90f68df451b8cb599f2430bd87e9c354a9ecbcb
Ivan <ivanborgo@outlook.com>
2023-07-08 18:42:01 -0300

    feat: updated userService interface

--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -1,10 +1,19 @@
 package com.api.crud.services;
 
 import com.api.crud.dtos.UserDTO;
+import com.api.crud.models.UserModel;
 import com.api.crud.view.UserView;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface UserService {
 
     UserView createUser(UserDTO user);
 
+    boolean existsByEmail(String email);
+
+    Optional<UserModel> findById(UUID id);
+
+    void deleteUser(UUID id);
 }
